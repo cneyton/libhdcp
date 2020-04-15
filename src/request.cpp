c18665b3b4d742dfd6f267a452236c9c54afef8e
cne <c.neyton@nateo-healthcare.com>
2020-04-15 16:08:50 +0200

    [REQUEST] add request
    Signed-off-by: cne <c.neyton@nateo-healthcare.com>

---
+++ src/request.cpp
@@ -0,0 +1,115 @@
+#include "request.h"
+#include "hdcp/exception.h"
+#include <mutex>
+
+using namespace hdcp;
+
+void RequestManager::send_command(Packet::BlockType type, std::string& data,
+                                  Request::Callback request_cb, std::chrono::milliseconds timeout)
+{
+    common::TimeoutQueue::Id id =
+        timeout_queue_.add_repeating(now_, timeout.count()/time_base_ms_,
+                           std::bind(&RequestManager::cmd_timeout_cb, this,
+                                     std::placeholders::_1, std::placeholders::_2));
+
+    // send command
+    Packet cmd = Packet::make_command(++packet_id_, type, data);
+    transport_.write(cmd.get_data());
+
+    // add request to the set
+    {
+        std::unique_lock<std::mutex> lk(requests_mutex_);
+        const auto [it, success] = requests_.insert({id, cmd, std::move(request_cb)});
+        if (!success)
+            throw hdcp::application_error("a request with the same packet id is pending");
+    }
+}
+
+void RequestManager::start_keepalive_management(std::chrono::milliseconds keepalive_interval,
+                                std::chrono::milliseconds keepalive_timeout)
+{
+    timeout_keepalive_ = keepalive_timeout.count()/time_base_ms_;
+    keepalive_mngt_id_ =
+        timeout_queue_.add_repeating(now_, keepalive_interval.count()/time_base_ms_,
+                                     std::bind(&RequestManager::ka_mngt_timeout_cb, this,
+                                               std::placeholders::_1, std::placeholders::_2));
+    // send keepalive
+    Packet ka = Packet::make_keepalive(++packet_id_);
+    transport_.write(ka.get_data());
+
+    ka_timeout_flag_ = false;
+}
+
+void RequestManager::stop_keepalive_management()
+{
+    timeout_queue_.erase(keepalive_mngt_id_);
+}
+
+void RequestManager::ack_command(Packet& packet)
+{
+    std::unique_lock<std::mutex> lk(requests_mutex_);
+    auto& set_by_command = requests_.get<by_command>();
+    auto search = set_by_command.find(packet.get_id());
+    if (search == set_by_command.end())
+        throw hdcp::application_error(fmt::format("request with packet id {} not found",
+                                                  packet.get_id()));
+    Request r(*search);
+    r.set_status(Request::Status::fulfilled);
+    r.set_ack(packet);
+    r.call_callback();
+
+    timeout_queue_.erase(r.get_id());
+    set_by_command.erase(packet.get_id());
+}
+
+void RequestManager::ack_keepalive()
+{
+    timeout_queue_.erase(keepalive_id_);
+}
+
+void RequestManager::cmd_timeout_cb(common::TimeoutQueue::Id id, int64_t now)
+{
+    std::unique_lock<std::mutex> lk(requests_mutex_);
+    auto& set_by_request = requests_.get<by_request>();
+    auto search = set_by_request.find(id);
+    if (search == set_by_request.end())
+        throw hdcp::application_error("request id not found, you should not be here");
+
+    if (search->get_retry() <= max_retry_) {
+        log_warn(logger_, "");
+        set_by_request.modify(search, std::bind(&Request::inc_retry, *search));
+        transport_.write(search->get_command().get_data());
+    } else {
+        log_error(logger_, "");
+        Request r(*search);
+        r.set_status(Request::Status::timeout);
+        r.call_callback();
+        timeout_queue_.erase(r.get_id());
+        set_by_request.erase(id);
+    }
+}
+
+void RequestManager::ka_timeout_cb(common::TimeoutQueue::Id id, int64_t now)
+{
+    ka_timeout_flag_ = true;
+}
+
+void RequestManager::ka_mngt_timeout_cb(common::TimeoutQueue::Id id, int64_t now)
+{
+    // send keepalive
+    Packet ka = Packet::make_keepalive(++packet_id_);
+    transport_.write(ka.get_data());
+    keepalive_id_ = timeout_queue_.add(now_, timeout_keepalive_,
+                                       std::bind(&RequestManager::ka_timeout_cb, this,
+                                                 std::placeholders::_1, std::placeholders::_2));
+}
+
+void RequestManager::run()
+{
+    now_ = 0;
+    packet_id_ = 0;
+    while (is_running()) {
+        timeout_queue_.run_once(now_++);
+        std::this_thread::sleep_for(std::chrono::milliseconds(time_base_ms_));
+    }
+}
---
+++ src/request.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#include <chrono>
+#include <atomic>
+
+#include <boost/multi_index/indexed_by.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/mem_fun.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index_container.hpp>
+
+#include "common/log.h"
+#include "common/timeout_queue.h"
+#include "common/thread.h"
+
+#include "hdcp/transport.h"
+#include "packet.h"
+
+namespace hdcp
+{
+
+class Request
+{
+public:
+    using Callback = std::function<void(Request&)>;
+
+    Request(common::TimeoutQueue::Id id, Packet& cmd, Callback callback):
+        id_(id), command_(cmd), cb_(callback) {}
+
+    virtual ~Request() {}
+
+    enum class Status {
+        pending,
+        timeout,
+        fulfilled,
+    };
+
+    Status                   get_status()     const {return status_;}
+    common::TimeoutQueue::Id get_id()         const {return id_;}
+    Packet::Id               get_command_id() const {return command_.get_id();}
+    const Packet&            get_command()    const {return command_;}
+    Packet*                  get_ack()        const {return ack_;}
+    uint                     get_retry()      const {return retry_;}
+
+    void set_status(Status s) {status_ = s;}
+    void set_ack(Packet& ack) {ack_ = &ack;}
+    void inc_retry()          {retry_++;}
+
+    void call_callback()      {cb_(*this);}
+
+private:
+    common::TimeoutQueue::Id  id_;
+    Packet                    command_;
+    Callback                  cb_;
+    Packet                  * ack_    = nullptr;
+    Status                    status_ = Status::pending;
+    uint                      retry_  = 0;
+};
+
+class RequestManager: public common::Log, public common::Thread
+{
+public:
+    RequestManager(common::Log logger, Transport& transport):
+        Log(logger), transport_(transport) {}
+    virtual ~RequestManager() {}
+
+    void send_command(Packet::BlockType type, std::string& data,
+                      Request::Callback request_cb, std::chrono::milliseconds timeout);
+
+    void start_keepalive_management(std::chrono::milliseconds keepalive_interval,
+                                    std::chrono::milliseconds keepalive_timeout);
+    void stop_keepalive_management();
+
+    void ack_command(Packet& packet);
+    void ack_keepalive();
+
+    bool keepalive_timeout() const {return ka_timeout_flag_;}
+
+private:
+    static const int64_t time_base_ms_ = 100;
+    static const uint    max_retry_    = 4;
+
+    struct by_request {};
+    struct by_command {};
+    typedef boost::multi_index_container<
+        Request,
+        boost::multi_index::indexed_by<
+            boost::multi_index::ordered_unique<
+            boost::multi_index::tag<by_request>,
+            boost::multi_index::const_mem_fun<Request, common::TimeoutQueue::Id, &Request::get_id>>,
+        boost::multi_index::ordered_unique<
+            boost::multi_index::tag<by_command>,
+            boost::multi_index::const_mem_fun<Request, Packet::Id, &Request::get_command_id>>>>
+            Set;
+
+    int64_t                 now_ = 0;
+    std::atomic<Packet::Id> packet_id_ = 0;
+
+    int64_t timeout_keepalive_;
+
+    Set requests_;
+    std::mutex requests_mutex_;
+
+    common::TimeoutQueue     timeout_queue_;
+    common::TimeoutQueue::Id keepalive_id_;
+    common::TimeoutQueue::Id keepalive_mngt_id_;
+    std::atomic_bool ka_timeout_flag_ = false;
+
+    Transport& transport_;
+
+    /*
+     * This cb resend a keep-alive every timeout_keepalive
+     */
+    void ka_mngt_timeout_cb(common::TimeoutQueue::Id id, int64_t now);
+
+    void cmd_timeout_cb(common::TimeoutQueue::Id id, int64_t now);
+    void ka_timeout_cb(common::TimeoutQueue::Id id, int64_t now);
+
+    virtual void run();
+};
+
+} /* namespace hdcp */
