329c7758667638b97752533fd3a4bf13fb9e57c7
cne <c.neyton@nateo-healthcare.com>
2020-09-16 16:46:28 +0200

    transport_error <- libusb_error & asio_error
    Signed-off-by: cne <c.neyton@nateo-healthcare.com>

--- include/hdcp/exception.h
+++ include/hdcp/exception.h
@@ -6,19 +6,37 @@
 
 #include "common/log.h"
 
-namespace hdcp
-{
-
-class libusb_error: public std::runtime_error
-{
-public:
-    libusb_error(int error_code): std::runtime_error(libusb_error_name(error_code)) {}
-};
+namespace hdcp {
 
 class transport_error: public std::runtime_error
 {
 public:
-    transport_error(const std::string& what_arg): std::runtime_error(what_arg) {}
+    enum Code {
+        write_queue_full,
+        read_queue_full,
+        not_permitted,
+        other,
+    };
+
+    transport_error(const std::string& what_arg, int code):
+        std::runtime_error(what_arg), code_(code) {}
+    int code() const {return code_;}
+
+private:
+    int code_;
+};
+
+class libusb_error: public transport_error
+{
+public:
+    libusb_error(int code): transport_error(libusb_error_name(code), code) {}
+};
+
+class asio_error: public transport_error
+{
+public:
+    asio_error(const boost::system::error_code& ec):
+        transport_error(fmt::format("{} ({})", ec.message(), ec.value()), ec.value()) {}
 };
 
 class application_error: public std::runtime_error
@@ -31,13 +49,6 @@ class packet_error: public std::runtime_error
 {
 public:
     packet_error(const std::string& what_arg): std::runtime_error(what_arg) {}
-};
-
-class asio_error: public std::runtime_error
-{
-public:
-    asio_error(const boost::system::error_code& ec):
-        std::runtime_error(fmt::format("{} ({})", ec.message(), ec.value())) {}
 };
 
 } /* namespace hdcp */
