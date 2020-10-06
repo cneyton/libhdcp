3e7ef6d93fd63a3bc826212b724d5600dba95d10
cne <c.neyton@nateo-healthcare.com>
2020-10-06 11:17:55 +0200

    [TEST] add cli to tcp tests

--- test/master_tcp_test.cpp
+++ test/master_tcp_test.cpp
@@ -3,6 +3,8 @@
 #include "hdcp/hdcp.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 
+#include "config.h"
+
 using namespace hdcp;
 
 static void data_cb(const Packet&)
@@ -10,41 +12,141 @@ static void data_cb(const Packet&)
     //std::this_thread::sleep_for(std::chrono::milliseconds(1));
 }
 
+class Cli: public common::Log, public common::Thread
+{
+public:
+    Cli(common::Logger logger, appli::Master& com): Log(logger), com_(com) {}
+    ~Cli()
+    {
+        com_.stop();
+        stop();
+        if (joinable())
+            join();
+    }
+
+    void start()
+    {
+        com_.start();
+        common::Thread::start(0);
+    }
+
+private:
+    using common::Thread::start;
+    appli::Master& com_;
+
+    void set_log_level()
+    {
+        int  choice = 254;
+        std::string usr_in;
+
+        do {
+            std::cout << "Select log level:\n"
+                " 0) trace\n"
+                " 1) debug\n"
+                " 2) info\n"
+                " 3) warning\n"
+                " 4) error\n"
+                " 5) critical\n"
+                ">> ";
+
+            std::cin >> usr_in;
+            try {
+                choice = std::stoi(usr_in);
+            } catch (...) {
+                choice = 254;
+            }
+
+            switch (choice) {
+            case 0: logger_->set_level(spdlog::level::trace); break;
+            case 1: logger_->set_level(spdlog::level::debug); break;
+            case 2: logger_->set_level(spdlog::level::info); break;
+            case 3: logger_->set_level(spdlog::level::warn); break;
+            case 4: logger_->set_level(spdlog::level::critical); break;
+            default: choice = 254; break;
+            }
+        } while(choice == 254);
+    };
+
+    void request_cb(Request&)
+    {
+    }
+
+    void test_command()
+    {
+        int  choice = 254;
+        std::string usr_in;
+
+        std::cout << "--------------------------------------------\n";
+        std::cout << "test " << com_.get_master_id() << "\n";
+        std::cout << "--------------------------------------------\n";
+        std::cout << "Select a command:\n"
+            "  0) connect\n"
+            "  1) disconnect\n"
+            "  2) send command\n"
+            "255) EXIT\n"
+            ">> ";
+        /* get user input */
+        std::cin >> usr_in;
+        if (usr_in == "l") {
+            set_log_level();
+            return;
+        }
+
+        try {
+            choice = std::stoi(usr_in);
+        } catch (...) {
+            log_warn(logger_, "invalid choice");
+            choice = 254;
+        }
+
+        switch (choice) {
+        case 0:
+            com_.connect();
+            break;
+        case 1:
+            com_.disconnect();
+            break;
+        case 2:
+            {
+                std::string cmd_data("test");
+                com_.send_command(0, cmd_data,
+                                  std::bind(&Cli::request_cb, this, std::placeholders::_1));
+                break;
+            }
+        case 255:
+            stop();
+            break;
+        default:
+            std::cout << "invalid choice: " << usr_in << std::endl;
+            break;
+        }
+    }
+
+    void run() override
+    {
+        while (is_running()) {
+            test_command();
+        }
+    }
+};
+
 int main(int argc, char* argv[])
 {
     if (argc != 3) {
         std::cerr << "usage: " << argv[0] << " <host> <port>" << std::endl;
         exit(EXIT_FAILURE);
     }
-    common::Logger logger(spdlog::stdout_color_mt("hdcp"));
+    common::Logger logger(spdlog::stderr_color_mt("hdcp"));
     logger->set_level(spdlog::level::debug);
 
-    Identification id {"client", "0001", "0.0.01", "0.1.02"};
-    appli::Master master(logger, id,
+    Identification id {"client", "NA", "NA", HDCP_VERSION};
+    appli::Master com(logger, id,
                   std::make_unique<transport::tcp::Client>(logger, argv[1], argv[2]));
-    master.set_data_cb(data_cb);
+    com.set_data_cb(data_cb);
 
-    master.start();
-    auto slave_id = master.connect();
+    Cli cli(logger, com);
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::string cmd_data("test");
-    master.send_command(0, cmd_data,
-                        [logger](Request& r)
-                        {
-                            switch (r.get_status()) {
-                            case Request::Status::pending:
-                                log_info(logger, "pending");
-                                break;
-                            case Request::Status::timeout:
-                                log_info(logger, "timeout");
-                                break;
-                            case Request::Status::fulfilled:
-                                log_info(logger, "fulfilled");
-                                break;
-                            }
-                        });
-
-    std::this_thread::sleep_for(std::chrono::seconds(30));
-    master.stop();
+    cli.start();
+    if (cli.joinable())
+        cli.join();
 }
--- test/slave_tcp_test.cpp
+++ test/slave_tcp_test.cpp
@@ -3,11 +3,137 @@
 #include "hdcp/hdcp.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 
+#include "config.h"
+
 using namespace hdcp;
 
 static void cmd_cb(const Packet&)
 {
 }
+
+class Cli: public common::Log, public common::Thread
+{
+public:
+    Cli(common::Logger logger, appli::Slave& com): Log(logger), com_(com) {}
+    ~Cli()
+    {
+        com_.stop();
+        stop();
+        if (joinable())
+            join();
+    }
+
+    void start()
+    {
+        com_.start();
+        common::Thread::start(0);
+    }
+
+private:
+    using common::Thread::start;
+    appli::Slave& com_;
+
+    void set_log_level()
+    {
+        int  choice = 254;
+        std::string usr_in;
+
+        do {
+            std::cout << "Select log level:\n"
+                " 0) trace\n"
+                " 1) debug\n"
+                " 2) info\n"
+                " 3) warning\n"
+                " 4) error\n"
+                " 5) critical\n"
+                ">> ";
+
+            std::cin >> usr_in;
+            try {
+                choice = std::stoi(usr_in);
+            } catch (...) {
+                choice = 254;
+            }
+
+            switch (choice) {
+            case 0: logger_->set_level(spdlog::level::trace); break;
+            case 1: logger_->set_level(spdlog::level::debug); break;
+            case 2: logger_->set_level(spdlog::level::info); break;
+            case 3: logger_->set_level(spdlog::level::warn); break;
+            case 4: logger_->set_level(spdlog::level::critical); break;
+            default: choice = 254; break;
+            }
+        } while(choice == 254);
+    };
+
+    void test_command()
+    {
+        int  choice = 254;
+        std::string usr_in;
+
+        std::cout << "--------------------------------------------\n";
+        std::cout << "test " << com_.get_slave_id() << "\n";
+        std::cout << "--------------------------------------------\n";
+        std::cout << "Select a command:\n"
+            "  0) connect\n"
+            "  1) disconnect\n"
+            "  2) send data\n"
+            "255) EXIT\n"
+            ">> ";
+        /* get user input */
+        std::cin >> usr_in;
+        if (usr_in == "l") {
+            set_log_level();
+            return;
+        }
+
+        try {
+            choice = std::stoi(usr_in);
+        } catch (...) {
+            log_warn(logger_, "invalid choice");
+            choice = 254;
+        }
+
+        switch (choice) {
+        case 0:
+            com_.wait_connected();
+            break;
+        case 1:
+            com_.disconnect();
+            break;
+        case 2:
+            {
+                std::string data(1000, 'a');
+                std::vector<hdcp::Packet::Block> blocks = {
+                    {
+                        .type = 0x2854,
+                        .data = data
+                    }
+                };
+                log_info(logger_, "start sending data...");
+                for (int i=0; i<20000; i++) {
+                    com_.send_data(blocks);
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                }
+                log_info(logger_, "20000 data sent...");
+                break;
+            }
+        case 255:
+            stop();
+            break;
+        default:
+            std::cout << "invalid choice: " << usr_in << std::endl;
+            break;
+        }
+    }
+
+    void run() override
+    {
+        while (is_running()) {
+            test_command();
+        }
+    }
+};
 
 int main(int argc, char* argv[])
 {
@@ -18,23 +144,14 @@ int main(int argc, char* argv[])
     common::Logger logger(spdlog::stdout_color_mt("hdcp"));
     logger->set_level(spdlog::level::debug);
 
-    Identification id {"server", "0001", "0.0.01", "0.1.02"};
+    Identification id {"server", "NA", "NA", HDCP_VERSION};
     uint16_t port = std::stoi(argv[1]);
-    appli::Slave slave(logger, id, std::make_unique<transport::tcp::Server>(logger, port));
-    slave.set_cmd_cb(cmd_cb);
+    appli::Slave com(logger, id, std::make_unique<transport::tcp::Server>(logger, port));
+    com.set_cmd_cb(cmd_cb);
 
-    slave.start();
-    slave.wait_connected();
-    std::string data(1000, 'a');
-    std::vector<hdcp::Packet::Block> blocks = {
-        {
-            .type = 0x2854,
-            .data = data
-        }
-    };
-    for (int i=0; i<20000; i++) {
-        slave.send_data(blocks);
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
-    slave.stop();
+    Cli cli(logger, com);
+
+    cli.start();
+    if (cli.joinable())
+        cli.join();
 }
